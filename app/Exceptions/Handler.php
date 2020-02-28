6806573530bd6ddde37cd9ff9548f591e0ae1744
carlos <carlossivera93@gmail.com>
2020-02-28 09:53:58 -0500

    Create Handler.php

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,82 @@
+<?php
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Exception;
+
+class Handler extends ExceptionHandler
+{
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [];
+    //
+    
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation'
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param \Exception $exception            
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request            
+     * @param \Exception $exception            
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        if ($this->isHttpException($exception)) {
+            switch ($exception->getStatusCode()) {
+                case 400:
+                    {
+                        return redirect()->route('400');
+                    }
+                    
+                    break;
+                case 403:
+                    {
+                        return redirect()->route('403');
+                    }
+                    break;
+                case 404:
+                    {
+                        return redirect()->route('404');
+                    }
+                    break;
+                case 500:
+                    
+                    {
+                        return redirect()->route('500');
+                    }
+                    
+                    break;
+            }
+        }
+        if($exception instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException){
+            return abort('404');
+        }
+        return parent::render($request, $exception);
+    }
+}
