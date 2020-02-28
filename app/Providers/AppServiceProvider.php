ccbec500274c59ac4eb60f9ccc55e88072895ce3
carlos <carlossivera93@gmail.com>
2020-02-28 09:43:02 -0500

    Create AppServiceProvider.php

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Menu;
+use App\Models\Usuario;
+
+/**
+ *
+ * @tutorial Working Class
+ * @author Bayron Tarazona ~bayronthz@gmail.com
+ * @since 06/05/2018
+ */
+class AppServiceProvider extends ServiceProvider
+{
+
+    /**
+     *
+     * @tutorial Method Description: Bootstrap any application services.
+     * @author Bayron Tarazona ~bayronthz@gmail.com
+     * @since {06/05/2018}
+     */
+    public function boot()
+    {
+        //view()->composer('layouts.admin', function ($view)
+        //{
+        //    $view->with('notificaciones', Usuario::notificaciones());
+        //});
+        Schema::defaultStringLength(200);
+    }
+
+    /**
+     *
+     * @tutorial Method Description: Register any application services.
+     * @author Bayron Tarazona ~bayronthz@gmail.com
+     * @since {06/05/2018}
+     */
+    public function register()
+    {
+        //
+    }
+}
