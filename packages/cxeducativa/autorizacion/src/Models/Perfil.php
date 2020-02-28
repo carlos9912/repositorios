b9c2521ff069b64ae7702d81fd22a2f355be4d1c
carlos <carlossivera93@gmail.com>
2020-02-28 09:55:14 -0500

    Create Perfil.php

---
+++ packages/cxeducativa/autorizacion/src/Models/Perfil.php
@@ -0,0 +1,138 @@
+<?php
+namespace Cxeducativa\Autorizacion\Models;
+
+use Cxeducativa\Autorizacion\Traits\PermisoTrait;
+use Illuminate\Database\Eloquent\Model;
+use Config;
+use Illuminate\Support\Facades\DB;
+
+/**
+ *
+ * @tutorial Working Class
+ * ;
+ * @since {24/05/2018}
+ */
+class Perfil extends Model
+{
+    use PermisoTrait {
+        flushPermisoCache as parentFlushPermisoCache;
+    }
+
+    /**
+     *
+     * @var string
+     */
+    protected $table = 'perfiles';
+
+    /**
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id_perfil';
+
+    /**
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'descripcion',
+        'especial',
+        'nombre',
+        'url'
+    ];
+
+    /**
+     *
+     * @tutorial Method Description: La etiqueta de caché cxeducativa utilizada por el modelo.
+     * ;
+     * @since {24/05/2018}
+     * @return string
+     */
+    public static function getShinobiTag()
+    {
+        return 'cxeducativa.perfiles';
+    }
+
+    /**
+     * Roles can belong to many users.
+     *
+     * @return Model
+     */
+    public function usuarios()
+    {
+        return $this->belongsToMany(config('auth.model') ?  : config('auth.providers.users.model'))->withTimestamps();
+    }
+
+    /**
+     * Get fresh permission slugs assigned to role from database.
+     *
+     * @return array
+     */
+    protected function getNuevosPermisos()
+    {
+        //dd($this->permisos);
+        $permisos=[];
+        foreach ($this->permisos as $permiso){
+            $permisos[]=$permiso->id_menu.".".$permiso->url;
+        }
+        return $permisos;
+    }
+
+    /**
+     * Flush the permission cache repository.
+     *
+     * @return void
+     */
+    public function flushPermissionCache()
+    {
+        $userClass = config('auth.model') ?  : config('auth.providers.users.model');
+        $usersTag = call_user_func([
+            $userClass,
+            'getAutorizacionTag'
+        ]);
+        static::parentFlushPermissionCache([
+            static::getAutorizacionTag(),
+            $usersTag
+        ]);
+    }
+
+    /**
+     * Checks if the role has the given permission.
+     *
+     * @param string $permission            
+     *
+     * @return bool
+     */
+    public function can($permission)
+    {
+        if ($this->special === 'no-access') {
+            return false;
+        }
+        
+        if ($this->special === 'all-access') {
+            return true;
+        }
+        
+        return $this->hasAllPermissions($permission, $this->getPermissions());
+    }
+
+    /**
+     * Check if the role has at least one of the given permissions.
+     *
+     * @param array $permission            
+     *
+     * @return bool
+     */
+    public function canAtLeast(array $permission = [])
+    {
+        if ($this->special === 'no-access') {
+            return false;
+        }
+        
+        if ($this->special === 'all-access') {
+            return true;
+        }
+        
+        return $this->hasAnyPermission($permission, $this->getPermissions());
+    }
+}
